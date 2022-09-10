d3cd2dfd20a9daa4919b37cef5fb512feee8b086
Chani <lsc90726@gmail.com>
2022-09-11 07:32:32 +0900

    12. Styling detail page & get data from mongoDB

--- client/src/page/details/Detail.js
+++ client/src/page/details/Detail.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import "../../styles/base.scss";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./detail.scss";
+
 const Detail = () => {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [toast, setToast] = useState({});
+  const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(-1);
+  };
   useEffect(() => {
     const getToastById = async () => {
       try {
-        const res = await axios.get("/toast/" + path);
+        const res = await axios.get("/api/toast/" + path);
         setToast(res.data);
         console.log(res.data);
       } catch (err) {
@@ -21,11 +26,13 @@ const Detail = () => {
   }, [path]);
   return (
     <div className="detail">
-      Detail
+      <h3>상세정보</h3>
       <img src={toast.img} alt="" />
-      <h3>{toast.title}</h3>
+      <h4 className="toast-title">{toast.title}</h4>
       <span>{toast.engtitle}</span>
       <p>{toast.desc}</p>
+
+      <button onClick={handleClick}>메뉴보기</button>
     </div>
   );
 };
